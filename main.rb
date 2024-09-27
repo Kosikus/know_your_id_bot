12a6dbd75456f21e6fb0fee1bbafaf2c99a83323
Constantin <ne.veshay.nos@gmail.com>
2024-09-27 20:08:50 +0300

    first stable version 1.0

---
+++ main.rb
@@ -0,0 +1,35 @@
+require 'telegram/bot'
+require 'dotenv' # для добавления в переменные окружения секретных данных
+
+# Загрузка переменных окружения (значение Token бота, имя и пароль к почте яндекса)
+Dotenv.load
+
+# Замените на ваш токен
+TOKEN = ENV['TELEGRAM_BOT_TOKEN']
+# Замените на ваш ID (ID вашего чата в боте)
+MY_CHAT_ID = '378860110'
+
+def log_user_data(user)
+  # Записываем данные пользователя в файл
+  File.open('./data/user_data.txt', 'a') do |file|
+    file.puts "ID: #{user.id}, Username: #{user.username || 'no username'}"
+  end
+end
+
+Telegram::Bot::Client.run(TOKEN) do |bot|
+  puts "Бот запущен..."
+
+  bot.listen do |message|
+    case message.text
+    when '/start'
+      user = message.from
+      log_user_data(user)
+      
+      # Сообщаем пользователю, что его данные сохранены
+      bot.api.send_message(chat_id: message.chat.id, text: "Привет, #{user.first_name}!\nВаши данные записаны ✍\nID: #{user.id}, Username: #{user.username || 'нет username'}")
+
+      # Отправляем данные пользователя в ваш чат
+      bot.api.send_message(chat_id: MY_CHAT_ID, text: "Новый пользователь: ID: #{user.id}, Username: #{user.username || 'нет username'}")
+    end
+  end
+end
